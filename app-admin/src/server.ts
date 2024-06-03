b40f5715c42ba0968a50b9791e872fd79901021c
Kane Xu <xu.hong@northeastern.edu>
2024-06-03 16:27:28 -0400

    init

--- app-admin/src/server.ts
+++ app-admin/src/server.ts
@@ -4,11 +4,6 @@ import payload from 'payload'
 require('dotenv').config()
 const app = express()
 
-app.use('/admin', (req, res, next) => {
-  req.url = req.url.replace(/^\/admin/, '');
-  next();
-});
-
 const start = async () => {
   // Initialize Payload
   await payload.init({
